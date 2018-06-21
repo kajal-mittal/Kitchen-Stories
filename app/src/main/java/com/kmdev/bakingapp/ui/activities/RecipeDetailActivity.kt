65f5f5445b98540eb019cdb064030678d7b43348
kajalmittal <kajal.mittal@punchh.com>
2018-06-21 23:21:54 +0530

    Changes into Recipe details. - add fonts - add styles.

--- app/src/main/java/com/kmdev/bakingapp/ui/activities/RecipeDetailActivity.kt
+++ app/src/main/java/com/kmdev/bakingapp/ui/activities/RecipeDetailActivity.kt
@@ -6,14 +6,22 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import com.google.gson.Gson
 
 import com.kmdev.bakingapp.R
 import com.kmdev.bakingapp.ui.models.RecipeDetailsModel
 import com.kmdev.bakingapp.utils.Constants
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
+import android.app.Activity
+import android.support.v7.widget.RecyclerView
 
-class RecipeDetailActivity : AppCompatActivity() {
+
+class RecipeDetailActivity : AppCompatActivity(),View.OnClickListener {
+
     var mRecipeString: String? = null
     private var mRecipeDetails: RecipeDetailsModel? = null
 
@@ -21,8 +29,11 @@ class RecipeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_detail)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
+        val publisherUrl = findViewById(R.id.tv_publisher_url) as TextView
         setSupportActionBar(toolbar)
         val fab = findViewById(R.id.fab) as FloatingActionButton
+        val webview = findViewById(R.id.webview) as WebView
+        val recyclerIngredients = findViewById(R.id.recycler_ingredients) as RecyclerView
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
@@ -30,7 +41,31 @@ class RecipeDetailActivity : AppCompatActivity() {
         mRecipeString = intent.getStringExtra(Constants.ARG_RECIPE_INFO)
         mRecipeDetails = Gson().fromJson<RecipeDetailsModel>(mRecipeString, RecipeDetailsModel::class.java!!)
         Toast.makeText(applicationContext, (mRecipeDetails as RecipeDetailsModel?)?.recipe?.title, Toast.LENGTH_SHORT).show()
+        publisherUrl?.setText((mRecipeDetails as RecipeDetailsModel?)?.recipe?.publisher)
+
+
+        webview.getSettings().setJavaScriptEnabled(true)
+
+        val activity = this
+        webview.setWebChromeClient(object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, progress: Int) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000)
+            }
+        })
+        webview.setWebViewClient(object : WebViewClient() {
+            override fun onReceivedError(view: WebView, errorCode: Int, description: String, failingUrl: String) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        webview.loadUrl((mRecipeDetails as RecipeDetailsModel)?.recipe?.source_url)
 
 
     }
+    override fun onClick(v: View?) {
+
+    }
+
 }
